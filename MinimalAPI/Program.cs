64a0a2b4775029212a6c2df9d90ab8e2ecc198cc
Diego-FSP <diegomgplus014@hotmail.com>
2025-07-08 12:06:03 -0300

    Minimal
    mas minimal que nunca

--- MinimalAPI/Program.cs
+++ MinimalAPI/Program.cs
@@ -1,6 +1,24 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Ghibli.PersistenciaDapper;
+using Ghibli.Persistencia;
+using Actores;
+using System.Data;
+using Microsoft.Extensions.Configuration;
+using MySqlConnector;
+using Scalar.AspNetCore;
 
-app.MapGet("/", () => "Hello World!");
+var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("MySQL");
+
+builder.Services.AddScoped<IDbConnection>(sp => new MySqlConnection(connectionString));
+
+//Cada vez que necesite la interfaz, se va a instanciar automaticamente AdoDapper y se va a pasar al metodo de la API
+builder.Services.AddScoped<RepoBase, RepoActor>();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+app.MapGet("/", () => "hola mundo");
 
 app.Run();
