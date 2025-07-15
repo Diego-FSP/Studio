e1d9b961d353e9bc843e62d3a21165c276f6c68a
Diego-FSP <diegomgplus014@hotmail.com>
2025-07-15 09:25:33 -0300

    al fin

--- MinimalAPI/Program.cs
+++ MinimalAPI/Program.cs
@@ -1,10 +1,9 @@
 using Ghibli.PersistenciaDapper;
-using Ghibli.Persistencia;
 using Actores;
 using System.Data;
-using Microsoft.Extensions.Configuration;
 using MySqlConnector;
 using Scalar.AspNetCore;
+using Ghibli.Persistencia;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,12 +12,25 @@ var connectionString = builder.Configuration.GetConnectionString("MySQL");
 builder.Services.AddScoped<IDbConnection>(sp => new MySqlConnection(connectionString));
 
 //Cada vez que necesite la interfaz, se va a instanciar automaticamente AdoDapper y se va a pasar al metodo de la API
-builder.Services.AddScoped<RepoBase, RepoActor>();
+builder.Services.AddScoped<IRepoActor, RepoActor>();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-app.MapGet("/", () => "hola mundo");
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger(options =>
+    {
+        options.RouteTemplate = "/openapi/{documentName}.json";
+    });
+    app.MapScalarApiReference();
+}
+
+app.MapGet("/Actor", async (IRepoActor actor) =>
+    await actor.ListarAsync());
+
+app.MapPost("/Actor/{ID}", async (ActorVoz nuevo , IRepoActor actor) =>
+await actor.AltaAsync(nuevo));
 
 app.Run();
