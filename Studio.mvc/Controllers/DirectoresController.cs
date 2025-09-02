1e9755a76ba5bce185488e0a2b27c8c7d1f1f258
Diego-FSP <diegomgplus014@hotmail.com>
2025-09-02 12:00:39 -0300

    Viva cristo rey

--- Studio.mvc/Controllers/DirectoresController.cs
+++ Studio.mvc/Controllers/DirectoresController.cs
@@ -1,5 +1,20 @@
+using System.Diagnostics;
+using Ghibli.Persistencia;
+using Ghibli.PersistenciaDapper;
+using Microsoft.AspNetCore.Mvc;
+using Studio.mvc.Models;
+
 namespace Studio.mvc.Controllers;
 
-public class DirectoresController
+public class DirectoresController : Controller
 {
+    IRepoDirector repo;
+    public DirectoresController(IRepoDirector director) => repo = director;
+    
+    public async Task<IActionResult> Listado()
+    {
+        var paises = await repo.ListarAsync();
+        return View(paises);
+    }
 }
+/* */
